7efbc9c6d1af0f9682725044883072cef13f121a
hanchang <hanchang_96@163.com>
2016-08-04 17:56:07 +0800

    add application - polls

---
+++ polls/models.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        # return u'%s'%(self.question_text)
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        # return u'%s'%(self.choice_text)
+        return self.choice_text
